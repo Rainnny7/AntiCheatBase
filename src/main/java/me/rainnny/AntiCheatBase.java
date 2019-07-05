59072f73f70cb186273e7aa54a4af2e073687a9f
Braydon <xcoolmanxx@gmail.com>
2019-07-05 19:04:40 -0400

    Initial commit

---
+++ src/main/java/me/rainnny/AntiCheatBase.java
@@ -0,0 +1,10 @@
+package me.rainnny;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AntiCheatBase extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // TODO
+    }
+}
